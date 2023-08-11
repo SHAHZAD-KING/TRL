e02615cc8eb583ac294660f03ee87c07d04cdbe1
SHAHZAD-KING <134486445+shahzadrahimy@users.noreply.github.com>
2023-08-11 17:24:36 +0430

    TRL.py
    rm -rf TRL
    
    git clone https://github.com/shahzadrahimy/TRL
    
    cd TRL
    
    python TRL.py

---
+++ TRL.py
@@ -0,0 +1,4 @@
+os.system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests ')
+os.system('pip install requests')
+os.system("pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests")
+import KING
