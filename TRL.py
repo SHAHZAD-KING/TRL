efae09e0f262ed0fa72995fdba7378e08df8e6ce
SHAHZAD-KING <134486445+shahzadrahimy@users.noreply.github.com>
2023-08-11 17:27:25 +0430

    Update TRL.py

--- TRL.py
+++ TRL.py
@@ -1,6 +1,1 @@
-import os
-import sys
-os.system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requests ')
-os.system('pip install requests')
-os.system("pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests")
 import KING
